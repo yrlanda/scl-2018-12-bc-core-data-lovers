bcc4d47af115d860742b71c3a9a93d371d9836fb
yrlanda <yrlaterivero@gmail.com>
2019-01-07 01:40:17 -0300

    agregar filtros y busquedas en la data

--- main.js
+++ main.js
@@ -1,22 +1,153 @@
-window.onload = () => {
-   const lista = document.getElementById('lista')
-    let i = 0;
+window.onscroll = function () { scrollFunction() };
 
-    for (var camp in LOL.data) {
-
-
-        if (i % 5 === 0) {
-            if (i > 0){
-                lista.appendChild(row);
-            }
-            var row = pintar_fila();
-            pintar_caja_inicial(LOL.data[camp]);
-        } else {
-            pintar_caja_estandar(LOL.data[camp]);
-        }
-        i++
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        document.getElementById("myBtn").style.display = "block";
+    } else {
+        document.getElementById("myBtn").style.display = "none";
     }
-    
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
+
+window.onload = () => {
+    let list = ordenar(LOL.data, 1);
+    draw_champions(list);
+    filter(list, "Mage");
+
+    document.getElementById('az').addEventListener('click', (evento) => {
+        evento.preventDefault();
+        clear_grid();
+        let list = ordenar(LOL.data, 1);
+        draw_champions(list);
+    });
+
+    document.getElementById('za').addEventListener('click', (evento) => {
+        evento.preventDefault();
+        clear_grid();
+        let list = ordenar(LOL.data, 2);
+        draw_champions(list);
+    });
+
+    document.getElementById('all').addEventListener('click', (evento) => {
+        clear_grid();
+        document.getElementById('categoria').textContent = "Champions";
+        let list = ordenar(LOL.data, 1);
+        draw_champions(list);
+    });
+
+    document.getElementById('mage').addEventListener('click', (evento) => {
+        evento.preventDefault();
+        clear_grid();
+        document.getElementById('categoria').textContent = "Mago";
+        let filtered_list = filter(list, "Mage");
+        draw_champions(filtered_list);
+    });
+
+    document.getElementById('tank').addEventListener('click', (evento) => {
+        evento.preventDefault();
+        document.getElementById('categoria').textContent = "Tanque";
+        clear_grid();
+        let filtered_list = filter(list, "Tank");
+        draw_champions(filtered_list);
+    });
+    document.getElementById('support').addEventListener('click', (evento) => {
+        evento.preventDefault();
+        clear_grid();
+        document.getElementById('categoria').textContent = "Apoyo";
+        let filtered_list = filter(list, "Support");
+        draw_champions(filtered_list);
+    });
+    document.getElementById('marksman').addEventListener('click', (evento) => {
+        evento.preventDefault();
+        clear_grid();
+        document.getElementById('categoria').textContent = "Tirador";
+        let filtered_list = filter(list, "Marksman");
+        draw_champions(filtered_list);
+    });
+    document.getElementById('assassin').addEventListener('click', (evento) => {
+        evento.preventDefault();
+        clear_grid();
+        document.getElementById('categoria').textContent = "Asesino";
+        let filtered_list = filter(list, "Assassin");
+        draw_champions(filtered_list);
+    });
+    document.getElementById('fighter').addEventListener('click', (evento) => {
+        evento.preventDefault();
+        clear_grid();
+        document.getElementById('categoria').textContent = "Guerrero";
+        let filtered_list = filter(list, "Fighter");
+        draw_champions(filtered_list);
+    });
+
+
+
+    function clear_grid() {
+        var myNode = document.getElementById('lista');
+        while (myNode.firstChild) {
+            myNode.removeChild(myNode.firstChild);
+        }
+    }
+
+    function ordenar(data, sort) {
+        const ordered = {};
+
+        if (sort == 1) {
+            Object.keys(data).sort().forEach(function (key) {
+                ordered[key] = data[key];
+            });
+        } else {
+            Object.keys(data).sort().reverse().forEach(function (key) {
+                ordered[key] = data[key];
+            });
+        }
+        return ordered
+
+    }
+
+    function checkClass(arr, val) {
+        return arr.some(function (arrVal) {
+            return val === arrVal;
+        });
+    }
+
+    function filter(data, filt) {
+        const filtered = {};
+
+        Object.keys(data).sort().forEach(function (key) {
+
+            if (checkClass(data[key].tags, filt)) {
+                filtered[key] = data[key];
+            }
+        });
+        return filtered
+    }
+
+    function draw_champions(list) {
+        const lista = document.getElementById('lista')
+        let i = 0;
+        console.log(typeof (list));
+
+        for (var champ in list) {
+            if (i % 5 === 0) {
+                if (i > 0) {
+                    lista.appendChild(row);
+                }
+                var row = pintar_fila();
+                pintar_caja_inicial(list[champ], row);
+            } else {
+                pintar_caja_estandar(list[champ], row);
+            }
+            i++
+        }
+        lista.appendChild(row);
+
+
+    }
 
     function pintar_fila() {
         var row = document.createElement('div');
@@ -24,9 +155,9 @@ window.onload = () => {
         return row;
     }
 
+    function pintar_caja_inicial(champion, row) {
 
-    function pintar_caja_inicial(champion) {
-        console.log(champion.title);
+        // console.log(champion.name);
 
         var div = document.createElement('div');
         div.className = 'col-sm-2 offset-sm-1';
@@ -58,9 +189,8 @@ window.onload = () => {
         row.appendChild(div);
     }
 
-
-    function pintar_caja_estandar(champion) {
-        console.log(champion.title);
+    function pintar_caja_estandar(champion, row) {
+        // console.log(champion.name);
 
         var div = document.createElement('div');
         div.className = 'col-sm-2';
@@ -78,7 +208,7 @@ window.onload = () => {
         var card_title = document.createElement('h5');
         card_title.className = 'card-title';
         card_title.textContent = champion.name;
-        
+
         var card_text = document.createElement('p');
         card_text.className = 'card-text';
         card_text.textContent = champion.title;
@@ -91,8 +221,5 @@ window.onload = () => {
         div.appendChild(card);
         row.appendChild(div);
 
-
     }
-
-
 }
