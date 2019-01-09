9ff29aa985b02bcdbee8316851b54be3a6925caa
yrlanda <yrlaterivero@gmail.com>
2019-01-08 23:17:59 -0300

    Separar funciones en data.js

--- data.js
+++ data.js
@@ -6,3 +6,42 @@ const example = () => {
 };
 
 window.example = example;
+
+const ordenar = (data, sort) => {
+  const ordered = {};
+
+  if (sort == 1) {
+    Object.keys(data).sort().forEach(function (key) {
+      ordered[key] = data[key];
+    });
+  } else {
+    Object.keys(data).sort().reverse().forEach(function (key) {
+      ordered[key] = data[key];
+    });
+  }
+  return ordered
+
+}
+
+window.ordenar = ordenar;
+
+const checkClass = (arr, val) => {
+  return arr.some(function (arrVal) {
+    return val === arrVal;
+  });
+
+}
+
+const filter = (data, filt) => {
+  const filtered = {};
+
+  Object.keys(data).sort().forEach(function (key) {
+
+    if (checkClass(data[key].tags, filt)) {
+      filtered[key] = data[key];
+    }
+  });
+  return filtered;
+
+}
+window.filter = filter;
